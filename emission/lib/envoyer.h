9bf8d4db20ce454d9e51cf7e1d5c237273a7ce81
Alexis Matarise <alexismatarise.btssn@gmail.com>
2025-03-25 14:25:49 +0100

    Creation fonction initialiserLiaisonSerie

---
+++ emission/lib/envoyer.h
@@ -0,0 +1,12 @@
+#ifndef ENVOYER_H
+#define ENVOYER_H
+
+#include <Arduino.h>
+
+#define VITESSE_DE_TRANSMISSION 9600
+
+void initialiserLiaisonSerie(u_int8_t vitesseDeTransmission = VITESSE_DE_TRANSMISSION);
+
+
+
+#endif
